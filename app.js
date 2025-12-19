d04c8b0013ab0f11dd6cb8b796d7f9808b405622
HrullekhaBhavana724 <bhavanapothuri818@gmail.com>
2025-12-19 20:57:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+//state
+let user__score=0;
+let computer__score=0;
+//selectors
+const hands=document.querySelectorAll(".main__hands img");
+const userScore=document.querySelector('.user__score');
+const computerScore=document.querySelector('.computer__score');
+const mainMessage=document.querySelector('.main__message');
+//get computerchoice
+const getComputerChoice=()=>{
+    const hands=["rock","paper","scissors"];
+    const computerChoice=hands[Math.floor(Math.random()*hands.length)];
+    return computerChoice;
+}
+//display message and update 
+const displayMessageAndUpdate=(e,condition,computerChoice)=>{
+    if(condition==="draw"){
+        //display message
+        mainMessage.textContent=`Draw! Both players selected ${e.target.id}.`
+        
+
+    }
+    if(condition==="win"){
+        //display message
+        mainMessage.textContent=`You win! ${e.target.id} beats ${computerChoice}.`
+        //update score
+        user__score++;
+        userScore.textContent=user__score;
+    }
+    if(condition==="lose"){
+        //display message
+        mainMessage.textContent=`You lose! ${computerChoice} beats ${e.target.id}.`
+        //update computerscore
+        computer__score++;
+        computerScore.textContent=computer__score;
+    }
+}
+
+//select choice
+const play=e=>{
+    //get computer choice
+const computerChoice=getComputerChoice();
+
+//check draw
+if(e.target.id==="rock"&&computerChoice==="rock"||e.target.id==="paper"&&computerChoice==="paper"||e.target.id==="scissors"&&computerChoice==="scissors"){
+    //display message and update score
+    displayMessageAndUpdate(e,"draw",computerChoice)
+}
+//check win
+if(e.target.id==="rock"&&computerChoice==="scissors"||e.target.id==="paper"&&computerChoice==="rock"||e.target.id==="scissors"&&computerChoice==="paper"){
+    //display message and update score
+    displayMessageAndUpdate(e,"win",computerChoice)
+}
+//check lose
+if(e.target.id==="rock"&&computerChoice==="paper"||e.target.id==="paper"&&computerChoice==="scissors"||e.target.id==="scissors"&&computerChoice==="rock"){
+    //display message and update score
+    displayMessageAndUpdate(e,"lose",computerChoice)
+}
+}
+hands.forEach(e=>{
+    e.addEventListener("click",play)
+})
+
+window.addEventListener("load",function(){
+    userScore.textContent=user__score;
+    computerScore.textContent=computer__score;
+})
